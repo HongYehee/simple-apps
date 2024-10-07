cdfc1582706ff7fb79c336ef73cd132a5545b565
yh.hong <ghddpgml@gmail.com>
2024-10-07 11:21:19 +0900

    App 20: Navigation & Tab bar

---
+++ 20-N&TProject/N&TProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N&TProject
+//
+//  Created by 홍예희 on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
